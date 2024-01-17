6802d4fff17667ffd5507571b833eb4e5b60e91c
Developer 2 <Developer 2@DESKTOP-8FFUO5K>
2024-01-17 10:15:01 +0330

    a

---
+++ Businesses Accounting/packages/n0.sql
@@ -0,0 +1,116 @@
+USE [master]
+GO
+
+/****** Object:  Database [BusinessesAccounting_db]    Script Date: 1/16/2024 5:01:51 PM ******/
+CREATE DATABASE [BusinessesAccounting_db]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'BusinessesAccounting_db', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA\BusinessesAccounting_db.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB ), 
+ FILEGROUP [AccountingData] 
+( NAME = N'Accounting1', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA\Accounting1' , SIZE = 131072KB , MAXSIZE = UNLIMITED, FILEGROWTH = 131072KB ), 
+ FILEGROUP [BusinessesData] 
+( NAME = N'Businesses1', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA\Businesses1' , SIZE = 131072KB , MAXSIZE = UNLIMITED, FILEGROWTH = 131072KB )
+ LOG ON 
+( NAME = N'BusinessesAccounting_db_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA\BusinessesAccounting_db_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
+ WITH CATALOG_COLLATION = DATABASE_DEFAULT
+GO
+
+
+
+ALTER DATABASE [BusinessesAccounting_db] SET ANSI_NULL_DEFAULT OFF 
+GO
+
+ALTER DATABASE [BusinessesAccounting_db] SET ANSI_NULLS OFF 
+GO
+
+ALTER DATABASE [BusinessesAccounting_db] SET ANSI_PADDING OFF 
+GO
+
+ALTER DATABASE [BusinessesAccounting_db] SET ANSI_WARNINGS OFF 
+GO
+
+ALTER DATABASE [BusinessesAccounting_db] SET ARITHABORT OFF 
+GO
+
+ALTER DATABASE [BusinessesAccounting_db] SET AUTO_CLOSE OFF 
+GO
+
+ALTER DATABASE [BusinessesAccounting_db] SET AUTO_SHRINK OFF 
+GO
+
+ALTER DATABASE [BusinessesAccounting_db] SET AUTO_UPDATE_STATISTICS ON 
+GO
+
+ALTER DATABASE [BusinessesAccounting_db] SET CURSOR_CLOSE_ON_COMMIT OFF 
+GO
+
+ALTER DATABASE [BusinessesAccounting_db] SET CURSOR_DEFAULT  GLOBAL 
+GO
+
+ALTER DATABASE [BusinessesAccounting_db] SET CONCAT_NULL_YIELDS_NULL OFF 
+GO
+
+ALTER DATABASE [BusinessesAccounting_db] SET NUMERIC_ROUNDABORT OFF 
+GO
+
+ALTER DATABASE [BusinessesAccounting_db] SET QUOTED_IDENTIFIER OFF 
+GO
+
+ALTER DATABASE [BusinessesAccounting_db] SET RECURSIVE_TRIGGERS OFF 
+GO
+
+ALTER DATABASE [BusinessesAccounting_db] SET  DISABLE_BROKER 
+GO
+
+ALTER DATABASE [BusinessesAccounting_db] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+GO
+
+ALTER DATABASE [BusinessesAccounting_db] SET DATE_CORRELATION_OPTIMIZATION OFF 
+GO
+
+ALTER DATABASE [BusinessesAccounting_db] SET TRUSTWORTHY OFF 
+GO
+
+ALTER DATABASE [BusinessesAccounting_db] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+GO
+
+ALTER DATABASE [BusinessesAccounting_db] SET PARAMETERIZATION SIMPLE 
+GO
+
+ALTER DATABASE [BusinessesAccounting_db] SET READ_COMMITTED_SNAPSHOT OFF 
+GO
+
+ALTER DATABASE [BusinessesAccounting_db] SET HONOR_BROKER_PRIORITY OFF 
+GO
+
+ALTER DATABASE [BusinessesAccounting_db] SET RECOVERY FULL 
+GO
+
+ALTER DATABASE [BusinessesAccounting_db] SET  MULTI_USER 
+GO
+
+ALTER DATABASE [BusinessesAccounting_db] SET PAGE_VERIFY CHECKSUM  
+GO
+
+ALTER DATABASE [BusinessesAccounting_db] SET DB_CHAINING OFF 
+GO
+
+ALTER DATABASE [BusinessesAccounting_db] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
+GO
+
+ALTER DATABASE [BusinessesAccounting_db] SET TARGET_RECOVERY_TIME = 60 SECONDS 
+GO
+
+ALTER DATABASE [BusinessesAccounting_db] SET DELAYED_DURABILITY = DISABLED 
+GO
+
+ALTER DATABASE [BusinessesAccounting_db] SET ACCELERATED_DATABASE_RECOVERY = OFF  
+GO
+
+ALTER DATABASE [BusinessesAccounting_db] SET QUERY_STORE = OFF
+GO
+
+ALTER DATABASE [BusinessesAccounting_db] SET  READ_WRITE 
+GO
+
+
